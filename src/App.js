b07292003fc0683b4ceceec5b59da06f41c1d198
jlclementjr <jlclementjr@yahoo.com>
2018-06-16 18:49:54 -0400

    pre eject

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -52,10 +51,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -75,10 +70,6 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
     }
 
     const classes = [];
@@ -92,7 +83,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
-        <p class={classes.join(' ')}>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} 
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
@@ -101,4 +92,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Person.css';
-import Radium from 'radium';
 
 const person = (props) => {
+
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
@@ -13,4 +13,4 @@ const person = (props) => {
 
 }
 
-export default Radium(person);
+export default person;
